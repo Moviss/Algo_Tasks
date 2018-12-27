6627520f74df52743cdb517d54373f2def54b605
Marcin Lubowicz <marcin.lubowicz@gmail.com>
2018-12-27 20:18:40 +0100

    Finished first task function

---
+++ task_1.py
@@ -0,0 +1,18 @@
+def zip_generator(first_zip, second_zip):
+    first_zip = int(first_zip.replace('-', ''))
+    second_zip = int(second_zip.replace('-', ''))
+    current_zip = first_zip
+    generated_zips = []
+    i = 0
+    while current_zip < second_zip-1:
+        current_zip += 1
+        current_zip_str = str(current_zip)
+        current_zip_str = current_zip_str[:2] + '-' + current_zip_str[2:]
+        generated_zips.append(current_zip_str)
+        i += i
+
+    return generated_zips
+
+# start = "79-900"
+# stop = "80-155"
+# print(zip_generator(start, stop))
