08e45cb22f14af41b5ce7d3f34b8d0cc5473afaa
Marcin Lubowicz <marcin.lubowicz@gmail.com>
2019-01-24 21:02:38 +0100

    Removed unecessary variable i

--- task_1.py
+++ task_1.py
@@ -3,15 +3,14 @@ def zip_generator(first_zip, second_zip):
     second_zip = int(second_zip.replace('-', ''))
     current_zip = first_zip
     generated_zips = []
-    i = 0
     while current_zip < second_zip-1:
         current_zip += 1
         current_zip_str = str(current_zip)
         current_zip_str = current_zip_str[:2] + '-' + current_zip_str[2:]
         generated_zips.append(current_zip_str)
-        i += i
 
     return generated_zips
+
 
 # start = "79-900"
 # stop = "80-155"
